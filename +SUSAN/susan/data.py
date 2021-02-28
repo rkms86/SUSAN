d38c65149200c59416ff94404f91186c3093e628
rkms96 <ricardo.msl.1986@gmail.com>
2021-02-28 23:57:06 +0100

    add python support

---
+++ +SUSAN/susan/data.py
@@ -0,0 +1,262 @@
+import struct, numpy, typing
+
+class particles:
+    def __init__(self, *args, **kwargs):
+        if len(args) == 1:
+            filename = args[0] 
+            self.load(filename)
+        else:
+            if len(args) == 3:
+                n_ptcl = args[0]
+                n_proj = args[1]
+                n_refs = args[2]
+                self.alloc(n_ptcl,n_proj,n_refs)
+            else:
+                raise NameError('Invalid input')
+
+    def load(self,filename):
+        fp = open(filename,"rb")
+        buffer = fp.read(8)
+        signature = buffer.decode("utf-8")
+        if signature != "SsaPtcl1" :
+            raise NameError("Invalid signature")
+        buffer = fp.read(4*3)
+        self.n_ptcl, self.n_proj, self.n_refs = struct.unpack('III',buffer)
+        self.__alloc_empty__()
+        n_bytes_per_ptcl = 4*( 10 + 8*self.n_refs + 7*self.n_proj + 8*self.n_proj )
+        for index in range(self.n_ptcl):
+            buffer = fp.read(n_bytes_per_ptcl)
+            self.__parse_buffer__(index,buffer)
+        fp.close()
+    
+    def alloc(self,n_ptcl,n_proj,n_refs):
+        self.n_ptcl = n_ptcl
+        self.n_proj = n_proj
+        self.n_refs = n_refs
+        self.__alloc_empty__()
+    
+    def save(self,filename):
+        fp = open(filename,"wb")
+        signature = "SsaPtcl1"
+        fp.write( bytearray(signature,'utf-8') )
+        fp.write( struct.pack('III',self.n_ptcl,self.n_proj,self.n_refs) )
+        for index in range(self.n_ptcl):
+            fp.write( struct.pack('III',self.ptcl_id[index],self.tomo_id[index],self.tomo_cix[index]) )
+            fp.write( struct.pack('fff',self.position[index,0],self.position[index,1],self.position[index,2]) )
+            fp.write( struct.pack('II' ,self.ref_cix[index],self.half_id[index] ) )
+            fp.write( struct.pack('ff' ,self.extra_1[index],self.extra_2[index] ) )
+            
+            # 3D alignment
+            for j in range(self.n_refs):
+                fp.write( struct.pack('fff' ,self.ali_eu[index,0,j],self.ali_eu[index,1,j],self.ali_eu[index,2,j] ) )
+            for j in range(self.n_refs):
+                fp.write( struct.pack('fff' ,self.ali_t[index,0,j],self.ali_t[index,1,j],self.ali_t[index,2,j] ) )
+            for j in range(self.n_refs):
+                fp.write( struct.pack('f' ,self.ali_cc[index,0,j] ) )
+            for j in range(self.n_refs):
+                fp.write( struct.pack('f' ,self.ali_w[index,0,j] ) )
+            
+            # 2D alignment
+            for j in range(self.n_proj):
+                fp.write( struct.pack('fff' ,self.prj_eu[index,0,j],self.prj_eu[index,1,j],self.prj_eu[index,2,j] ) )
+            for j in range(self.n_proj):
+                fp.write( struct.pack('ff' ,self.prj_t[index,0,j],self.prj_t[index,1,j] ) )
+            for j in range(self.n_proj):
+                fp.write( struct.pack('f' ,self.prj_cc[index,0,j] ) )
+            for j in range(self.n_proj):
+                fp.write( struct.pack('f' ,self.prj_w[index,0,j] ) )
+            
+            # Defocus
+            for j in range(self.n_proj):
+                fp.write( struct.pack('fff' ,self.def_U[index,j],self.def_V[index,j],self.def_ang[index,j] ) )
+                fp.write( struct.pack('fff' ,self.def_phas[index,j],self.def_Bfct[index,j],self.def_ExFl[index,j] ) )
+                fp.write( struct.pack('ff'  ,self.def_mres[index,j],self.def_scor[index,j] ) )
+        fp.close()
+    
+    def __getitem__(self,idx):
+        tmp = self.ptcl_id[idx]
+        ptcls_out = particles(tmp.shape[0],self.n_proj,self.n_refs)
+        ptcls_out.ptcl_id  = self.ptcl_id [idx]
+        ptcls_out.tomo_id  = self.tomo_id [idx]
+        ptcls_out.tomo_cix = self.tomo_cix[idx]
+        ptcls_out.position = self.position[idx,:]
+        ptcls_out.ref_cix  = self.ref_cix[idx]
+        ptcls_out.half_id  = self.half_id[idx]
+        ptcls_out.extra_1  = self.extra_1[idx]
+        ptcls_out.extra_2  = self.extra_2[idx]
+        # 3D alignment
+        ptcls_out.ali_eu   = self.ali_eu[idx,:,:]
+        ptcls_out.ali_t    = self.ali_t [idx,:,:]
+        ptcls_out.ali_cc   = self.ali_cc[idx,:,:]
+        ptcls_out.ali_w    = self.ali_w [idx,:,:]
+        # 2D alignment
+        ptcls_out.prj_eu   = self.prj_eu[idx,:,:]
+        ptcls_out.prj_t    = self.prj_t [idx,:,:]
+        ptcls_out.prj_cc   = self.prj_cc[idx,:,:]
+        ptcls_out.prj_w    = self.prj_w [idx,:,:]
+        # Defocus
+        ptcls_out.def_U    = self.def_U   [idx,:]
+        ptcls_out.def_V    = self.def_V   [idx,:]
+        ptcls_out.def_ang  = self.def_ang [idx,:]
+        ptcls_out.def_phas = self.def_phas[idx,:]
+        ptcls_out.def_Bfct = self.def_Bfct[idx,:]
+        ptcls_out.def_ExFl = self.def_ExFl[idx,:]
+        ptcls_out.def_mres = self.def_mres[idx,:]
+        ptcls_out.def_scor = self.def_scor[idx,:]
+        return ptcls_out
+    
+    def select(self,idx):
+        w_idx = idx[:,0]
+        tmp = self.ptcl_id[w_idx]
+        ptcls_out = particles(tmp.shape[0],self.n_proj,self.n_refs)
+        ptcls_out.ptcl_id  = self.ptcl_id [w_idx]
+        ptcls_out.tomo_id  = self.tomo_id [w_idx]
+        ptcls_out.tomo_cix = self.tomo_cix[w_idx]
+        ptcls_out.position = self.position[w_idx,:]
+        ptcls_out.ref_cix  = self.ref_cix[w_idx]
+        ptcls_out.half_id  = self.half_id[w_idx]
+        ptcls_out.extra_1  = self.extra_1[w_idx]
+        ptcls_out.extra_2  = self.extra_2[w_idx]
+        # 3D alignment
+        ptcls_out.ali_eu   = self.ali_eu[w_idx,:,:]
+        ptcls_out.ali_t    = self.ali_t [w_idx,:,:]
+        ptcls_out.ali_cc   = self.ali_cc[w_idx,:,:]
+        ptcls_out.ali_w    = self.ali_w [w_idx,:,:]
+        # 2D alignment
+        ptcls_out.prj_eu   = self.prj_eu[w_idx,:,:]
+        ptcls_out.prj_t    = self.prj_t [w_idx,:,:]
+        ptcls_out.prj_cc   = self.prj_cc[w_idx,:,:]
+        ptcls_out.prj_w    = self.prj_w [w_idx,:,:]
+        # Defocus
+        ptcls_out.def_U    = self.def_U   [w_idx,:]
+        ptcls_out.def_V    = self.def_V   [w_idx,:]
+        ptcls_out.def_ang  = self.def_ang [w_idx,:]
+        ptcls_out.def_phas = self.def_phas[w_idx,:]
+        ptcls_out.def_Bfct = self.def_Bfct[w_idx,:]
+        ptcls_out.def_ExFl = self.def_ExFl[w_idx,:]
+        ptcls_out.def_mres = self.def_mres[w_idx,:]
+        ptcls_out.def_scor = self.def_scor[w_idx,:]
+        return ptcls_out
+    
+    def __alloc_empty__(self):
+        self.ptcl_id  = numpy.zeros(self.n_ptcl,dtype=numpy.uint32)
+        self.tomo_id  = numpy.zeros(self.n_ptcl,dtype=numpy.uint32)
+        self.tomo_cix = numpy.zeros(self.n_ptcl,dtype=numpy.uint32)
+        self.position = numpy.zeros((self.n_ptcl,3),dtype=numpy.float32) # in Angstroms
+        self.ref_cix  = numpy.zeros(self.n_ptcl,dtype=numpy.uint32)
+        self.half_id  = numpy.zeros(self.n_ptcl,dtype=numpy.uint32)
+        self.extra_1  = numpy.zeros(self.n_ptcl,dtype=numpy.float32)
+        self.extra_2  = numpy.zeros(self.n_ptcl,dtype=numpy.float32)
+        
+        # 3D alignment
+        self.ali_eu   = numpy.zeros((self.n_ptcl,3,self.n_refs),dtype=numpy.float32) # in Radians
+        self.ali_t    = numpy.zeros((self.n_ptcl,3,self.n_refs),dtype=numpy.float32) # in Angstroms
+        self.ali_cc   = numpy.zeros((self.n_ptcl,1,self.n_refs),dtype=numpy.float32)
+        self.ali_w    = numpy.zeros((self.n_ptcl,1,self.n_refs),dtype=numpy.float32)
+        
+        # 2D alignment
+        self.prj_eu   = numpy.zeros((self.n_ptcl,3,self.n_proj),dtype=numpy.float32) # in Radians
+        self.prj_t    = numpy.zeros((self.n_ptcl,2,self.n_proj),dtype=numpy.float32) # in Angstroms
+        self.prj_cc   = numpy.zeros((self.n_ptcl,1,self.n_proj),dtype=numpy.float32)
+        self.prj_w    = numpy.zeros((self.n_ptcl,1,self.n_proj),dtype=numpy.float32)
+        
+        # Defocus
+        self.def_U    = numpy.zeros((self.n_ptcl,self.n_proj),dtype=numpy.float32) # U (angstroms)
+        self.def_V    = numpy.zeros((self.n_ptcl,self.n_proj),dtype=numpy.float32) # V (angstroms)
+        self.def_ang  = numpy.zeros((self.n_ptcl,self.n_proj),dtype=numpy.float32) # angles (sexagesimal)
+        self.def_phas = numpy.zeros((self.n_ptcl,self.n_proj),dtype=numpy.float32) # phase shift (sexagesimal?)
+        self.def_Bfct = numpy.zeros((self.n_ptcl,self.n_proj),dtype=numpy.float32) # Bfactor
+        self.def_ExFl = numpy.zeros((self.n_ptcl,self.n_proj),dtype=numpy.float32) # Exposure filter
+        self.def_mres = numpy.zeros((self.n_ptcl,self.n_proj),dtype=numpy.float32) # Max. resolution (angstroms)
+        self.def_scor = numpy.zeros((self.n_ptcl,self.n_proj),dtype=numpy.float32) # score
+        
+    def __parse_buffer__(self,index,buffer):
+        i=0
+        self.ptcl_id [index], self.tomo_id [index], self.tomo_cix[index], \
+        self.position[index,0], self.position[index,1], self.position[index,2], \
+        self.ref_cix [index], self.half_id [index], \
+        self.extra_1 [index], self.extra_2 [index] =  struct.unpack('IIIfffIIff',buffer[i:i+40])
+        i=i+40
+        
+        # 3D alignment
+        for j in range(self.n_refs):
+            self.ali_eu[index,0:3,j] = struct.unpack('fff',buffer[i:i+12])
+            i=i+12
+        for j in range(self.n_refs):
+            self.ali_t [index,0:3,j] = struct.unpack('fff',buffer[i:i+12])
+            i=i+12
+        for j in range(self.n_refs):
+            self.ali_cc[index,0,j] = struct.unpack('f',buffer[i:i+4])[0]
+            i=i+4;
+        for j in range(self.n_refs):
+            self.ali_w [index,0,j] = struct.unpack('f',buffer[i:i+4])[0]
+            i=i+4;
+        
+        # 2D alignment
+        for j in range(self.n_proj):
+            self.prj_eu[index,0:3,j] = struct.unpack('fff',buffer[i:i+12])
+            i=i+12
+        for j in range(self.n_proj):
+            self.prj_t [index,0:2,j] = struct.unpack('ff',buffer[i:i+8])
+            i=i+8
+        for j in range(self.n_proj):
+            self.prj_cc[index,0,j] = struct.unpack('f',buffer[i:i+4])[0]
+            i=i+4
+        for j in range(self.n_proj):
+            self.prj_w [index,0,j] = struct.unpack('f',buffer[i:i+4])[0]
+            i=i+4
+        
+        # Defocus
+        for j in range(self.n_proj):
+            self.def_U[index,j], self.def_V[index,j], self.def_ang[index,j], \
+            self.def_phas[index,j], self.def_Bfct[index,j], self.def_ExFl[index,j], \
+            self.def_mres[index,j], self.def_scor[index,j] = struct.unpack('ffffffff',buffer[i:i+32])
+            i=i+32
+
+def read_value(fp,tag):
+    line = fp.readline().strip().decode('utf-8')
+    while len(line) > 0 and line[0] == "#" :
+        line = fp.readline().strip().decode('utf-8')
+    if not line.startswith(tag):
+        raise NameError("Requested field "+tag+", but the line is "+line)
+    return line[(len(tag)+1):]
+
+def write_value(fp,tag,value):
+    fp.write(tag+':'+value+'\n')
+    
+class reference(typing.NamedTuple):
+    ref : str
+    mask : str
+    h1 : str
+    h2 : str
+
+def load_references(filename):
+    fp = open(filename,"rb")
+    num_refs = int(read_value(fp,'num_ref'))
+    names=[]
+    masks=[]
+    half1=[]
+    half2=[]
+    for i in range(num_refs):
+        names.append(read_value(fp,'map'))
+        masks.append(read_value(fp,'mask'))
+        half1.append(read_value(fp,'h1'))
+        half2.append(read_value(fp,'h2'))
+    fp.close()
+    result = [
+        reference(*refs_details)
+        for refs_details in zip(names, masks, half1, half2)
+    ]
+    return result
+
+def save_references(refs,filename):
+    fp=open(filename,'w')
+    write_value(fp,'num_refs',str(len(refs)))
+    for i in range(len(refs)):
+        fp.write('## Reference '+str(i+1)+'\n')
+        write_value(fp,'map',refs[i].ref)
+        write_value(fp,'mask',refs[i].mask)
+        write_value(fp,'h1',refs[i].h1)
+        write_value(fp,'h2',refs[i].h2)
+    fp.close()
+
---
+++ +SUSAN/susan/modules.py
@@ -0,0 +1,249 @@
+import os,susan,math
+
+def get_gpu_str(list_gpus_ids):
+    gpu_str = ''
+    for i in range(len(list_gpus_ids)):
+        gpu_str = gpu_str + '%d,' % list_gpus_ids[i]
+    gpu_str = gpu_str[0:-1]
+    return gpu_str
+
+class aligner:
+    def __init__(self):
+        self.list_gpus_ids     = [0]
+        self.threads_per_gpu   = 1
+        self.bandpass_highpass = 0
+        self.bandpass_lowpass  = 20
+        self.bandpass_rolloff  = 3
+        self.dimensionality    = 3
+        self.extra_padding     = 0
+        self.allow_drift       = True
+        self.halfsets_independ = False
+        self.cone_range        = 0
+        self.cone_step         = 1
+        self.inplane_range     = 0
+        self.inplane_step      = 1
+        self.refine_level      = 0
+        self.refine_factor     = 1
+        self.offset_range      = [4,4,4]
+        self.offset_step       = 1
+        self.offset_type       = 'ellipsoid'
+        self.padding_type      = 'noise'
+        self.normalize_type    = 'zero_mean'
+        self.ctf_correction    = 'on_substack'
+        self.pseudo_symmetry   = 'c1'
+        self.ssnr_S            = 0
+        self.ssnr_F            = 0
+        
+    def set_angular_search(self,c_r=0,c_s=1,i_r=0,i_s=1):
+        self.cone_range    = c_r
+        self.cone_step     = c_s
+        self.inplane_range = i_r
+        self.inplane_step  = i_s
+        
+    def set_offset_search(self,off_range,off_step,off_type='ellipsoid'):
+        if not off_type in ['ellipsoid','cylinider']:
+            raise NameError('Invalid offset type. Only "ellipsoid" or "cylinder" are valid')
+        self.offset_range = off_range
+        self.offset_step  = off_step
+        self.offset_type  = off_type
+        
+    def validate(self):
+        if not self.dimensionality in [2,3]:
+            raise NameError('Invalid dimensionality type. Only 3 or 3 are valid')
+        
+        if not self.offset_type in ['ellipsoid','cylinider']:
+            raise NameError('Invalid offset type. Only "ellipsoid" or "cylinder" are valid')
+        
+        if not self.padding_type in ['zero','noise']:
+            raise NameError('Invalid padding type. Only "zero" or "noise" are valid')
+        
+        if not self.normalize_type in ['none','zero_mean','zero_mean_one_weight','zero_mean_proj_weight']:
+            raise NameError('Invalid normalization type. Only "none", "zero_mean", "zero_mean_one_weight" or "zero_mean_proj_weight" are valid')
+        
+        if not self.ctf_correction in ['none','on_reference','on_substack','wiener_ssnr','wiener_white']:
+            raise NameError('Invalid ctf correction type. Only "none", "on_reference", "on_substack", "wiener_ssnr" or "wiener_white" are valid')
+            
+    def get_args(self,ptcls_out,refs_file,tomos_file,ptcls_in,box_size):
+        self.validate()
+        n_threads = '%d' % (len(self.list_gpus_ids)*self.threads_per_gpu)
+        gpu_str = get_gpu_str(self.list_gpus_ids)
+        args =        ' -tomos_file ' + tomos_file
+        args = args + ' -ptcls_in '   + ptcls_in
+        args = args + ' -ptcls_out '  + ptcls_out
+        args = args + ' -refs_file '  + refs_file
+        args = args + ' -n_threads '  + n_threads
+        args = args + ' -gpu_list '   + gpu_str
+        args = args + ' -box_size %d' % box_size
+        args = args + ' -pad_size %d' % self.extra_padding
+        args = args + ' -pad_type '   + self.padding_type
+        args = args + ' -norm_type '  + self.normalize_type
+        args = args + ' -ctf_type '   + self.ctf_correction
+        args = args + ' -ssnr_param ' + ('%f' % self.ssnr_F) + (',%f' % self.ssnr_S)
+        args = args + ' -bandpass '   + ('%f' % self.bandpass_highpass) + (',%f' % self.bandpass_lowpass)
+        args = args + ' -rolloff_f '  + ('%f' % self.bandpass_rolloff)
+        args = args + ' -p_symmetry ' + self.pseudo_symmetry
+        if self.halfsets_independ:
+            args = args + ' -ali_halves 1'
+        else:
+            args = args + ' -ali_halves 0'
+        if self.allow_drift:
+            args = args + ' -allow_drift 1'
+        else:
+            args = args + ' -allow_drift 0'
+        args = args + ' -cone '       + ('%f' % self.cone_range) + (',%f' % self.cone_step)
+        args = args + ' -inplane '    + ('%f' % self.inplane_range) + (',%f' % self.inplane_step)
+        args = args + ' -refine '     + ('%f' % self.refine_factor) + (',%f' % self.refine_level)
+        args = args + ' -off_type '   + self.offset_type
+        args = args + ' -off_params ' + ('%f' % self.offset_range[0]) + (',%f' % self.offset_range[1]) + (',%f' % self.offset_range[2]) + (',%f' % self.offset_step)
+        args = args + ' -type %d'     % self.dimensionality
+        return args
+    
+    def align(self,ptcls_out,refs_file,tomos_file,ptcls_in,box_size):
+        cmd = os.path.dirname(susan.__file__)+'/../bin/susan_aligner'
+        cmd = cmd + self.get_args(ptcls_out, refs_file, tomos_file, ptcls_in, box_size)
+        rslt = os.system(cmd)
+        if not rslt == 0:
+            raise NameError('Error executing the alignment: ' + cmd)
+    
+    def align_mpi(self,ptcls_out,refs_file,tomos_file,ptcls_in,box_size,mpi_nodes):
+        cmd = 'mpirun -n ' + ('%d ' % mpi_nodes)
+        cmd = cmd + os.path.dirname(susan.__file__)+'/../bin/susan_aligner_mpi'
+        cmd = cmd + self.get_args(ptcls_out, refs_file, tomos_file, ptcls_in, box_size)
+        rslt = os.system(cmd)
+        if not rslt == 0:
+            raise NameError('Error executing the alignment: ' + cmd)
+        
+class averager:
+    def __init__(self):
+        self.list_gpus_ids     = [0]
+        self.threads_per_gpu   = 1
+        self.bandpass_highpass = 0
+        self.bandpass_lowpass  = 20
+        self.bandpass_rolloff  = 3
+        self.extra_padding     = 0
+        self.rec_halfsets      = False
+        self.padding_type      = 'noise'
+        self.normalize_type    = 'zero_mean'
+        self.ctf_correction    = 'wiener'
+        self.symmetry          = 'c1'
+        self.ssnr_S            = 0
+        self.ssnr_F            = 0
+        self.inversion_iter    = 10
+        self.inversion_gstd    = 0.75
+        
+    def validate(self):
+        if not self.padding_type in ['zero','noise']:
+            raise NameError('Invalid padding type. Only "zero" or "noise" are valid')
+        
+        if not self.normalize_type in ['none','zero_mean','zero_mean_one_weight','zero_mean_proj_weight']:
+            raise NameError('Invalid normalization type. Only "none", "zero_mean", "zero_mean_one_weight" or "zero_mean_proj_weight" are valid')
+        
+        if not self.ctf_correction in ['none','phase_flip','wiener','wiener_ssnr']:
+            raise NameError('Invalid ctf correction type. Only "none", "phase_flip", "wiener" ot "wiener_ssnr" are valid')
+            
+    def get_args(self,out_pfx,tomos_file,ptcls_in,box_size):
+        self.validate()
+        n_threads = '%d' % (len(self.list_gpus_ids)*self.threads_per_gpu)
+        gpu_str = get_gpu_str(self.list_gpus_ids)
+        args =        ' -tomos_file ' + tomos_file
+        args = args + ' -out_prefix ' + out_pfx
+        args = args + ' -ptcls_file ' + ptcls_in
+        args = args + ' -n_threads '  + n_threads
+        args = args + ' -gpu_list '   + gpu_str
+        args = args + ' -box_size %d' % box_size
+        args = args + ' -pad_size %d' % self.extra_padding
+        args = args + ' -pad_type '   + self.padding_type
+        args = args + ' -norm_type '  + self.normalize_type
+        args = args + ' -ctf_type '   + self.ctf_correction
+        args = args + ' -ssnr_param ' + ('%f' % self.ssnr_F) + (',%f' % self.ssnr_S)
+        args = args + ' -w_inv_iter ' + ('%d' % self.inversion_iter)
+        args = args + ' -w_inv_gstd ' + ('%f' % self.inversion_gstd)
+        args = args + ' -bandpass '   + ('%f' % self.bandpass_highpass) + (',%f' % self.bandpass_lowpass)
+        args = args + ' -rolloff_f '  + ('%f' % self.bandpass_rolloff)
+        args = args + ' -symmetry '   + self.symmetry
+        if self.rec_halfsets:
+            args = args + ' -rec_halves 1'
+        else:
+            args = args + ' -rec_halves 0'
+        return args
+    
+    def reconstruct(self,out_pfx,tomos_file,ptcls_in,box_size):
+        cmd = os.path.dirname(susan.__file__)+'/../bin/susan_reconstruct'
+        cmd = cmd + self.get_args(out_pfx,tomos_file,ptcls_in,box_size)
+        rslt = os.system(cmd)
+        if not rslt == 0:
+            raise NameError('Error executing the reconstruction: ' + cmd)
+    
+    def reconstruct_mpi(self,out_pfx,tomos_file,ptcls_in,box_size,mpi_nodes):
+        cmd = 'mpirun -n ' + ('%d ' % mpi_nodes)
+        cmd = cmd + os.path.dirname(susan.__file__)+'/../bin/susan_reconstruct_mpi'
+        cmd = cmd + self.get_args(out_pfx,tomos_file,ptcls_in,box_size)
+        rslt = os.system(cmd)
+        if not rslt == 0:
+            raise NameError('Error executing the reconstruction: ' + cmd)
+        
+class ref_fsc:
+    def __init__(self):
+        self.gpu_id    = 0
+        self.rand_fpix = 15
+        self.pix_size  = 1
+        self.threshold = 0.143
+        self.save_svg  = False
+        
+    def calculate(self,out_dir,refs_file):
+        cmd = os.path.dirname(susan.__file__)+'/../bin/susan_refs_fsc'
+        cmd = cmd + ' -out_dir '   + out_dir
+        cmd = cmd + ' -refs_file ' + refs_file
+        cmd = cmd + ' -gpu_id '    + ('%d' % self.gpu_id)
+        cmd = cmd + ' -rand_fpix ' + ('%f' % self.rand_fpix)
+        cmd = cmd + ' -pix_size '  + ('%f' % self.pix_size)
+        cmd = cmd + ' -threshold ' + ('%f' % self.threshold)
+        if self.save_svg:
+            cmd = cmd + ' -save_svg 1'
+        else:
+            cmd = cmd + ' -save_svg 0'
+        rslt = os.system(cmd)
+        if not rslt == 0:
+            raise NameError('Error calculating the FSCs: ' + cmd)
+    
+class ref_align:
+    def __init__(self):
+        self.gpu_id            = 0
+        self.bandpass_highpass = 0
+        self.bandpass_lowpass  = 20
+        self.bandpass_rolloff  = 3
+        self.cone_range        = 0
+        self.cone_step         = 1
+        self.inplane_range     = 0
+        self.inplane_step      = 1
+        self.refine_level      = 0
+        self.refine_factor     = 1
+        self.offset_range      = [4,4,4]
+        self.offset_step       = 1
+        self.offset_type       = 'ellipsoid'
+        self.pix_size          = 1
+        
+    def validate(self):
+        if not self.offset_type in ['ellipsoid','cylinider']:
+            raise NameError('Invalid offset type. Only "ellipsoid" or "cylinder" are valid')
+        
+    def align(self,ptcls_out,refs_file,ptcls_in,box_size):
+        self.validate()
+        cmd = os.path.dirname(susan.__file__)+'/../bin/susan_refs_aligner'
+        cmd = cmd + ' -ptcls_in '   + ptcls_in
+        cmd = cmd + ' -ptcls_out '  + ptcls_out
+        cmd = cmd + ' -refs_file '  + refs_file
+        cmd = cmd + ' -gpu_id '     + ('%d' % self.gpu_id)
+        cmd = cmd + ' -box_size '   + ('%d' % box_size)
+        cmd = cmd + ' -bandpass '   + ('%f' % self.bandpass_highpass) + (',%f' % self.bandpass_lowpass)
+        cmd = cmd + ' -rolloff_f '  + ('%f' % self.bandpass_rolloff)
+        cmd = cmd + ' -cone '       + ('%f' % self.cone_range) + (',%f' % self.cone_step)
+        cmd = cmd + ' -inplane '    + ('%f' % self.inplane_range) + (',%f' % self.inplane_step)
+        cmd = cmd + ' -refine '     + ('%f' % self.refine_factor) + (',%f' % self.refine_level)
+        cmd = cmd + ' -off_type '   + self.offset_type
+        cmd = cmd + ' -off_params ' + ('%f' % self.offset_range[0]) + (',%f' % self.offset_range[1]) + (',%f' % self.offset_range[2]) + (',%f' % self.offset_step)
+        cmd = cmd + ' -pix_size '   + ('%f' % self.pix_size)
+        rslt = os.system(cmd)
+        if not rslt == 0:
+            raise NameError('Error calculating the FSCs: ' + cmd)
+    
