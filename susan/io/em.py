65362121bc743fca448dcaea2175139f81dd2758
Ricardo Miguel Sanchez Loayza <ricardo.msl.1986@gmail.com>
2023-02-23 09:49:38 +0100

    Add read EM support

--- susan/io/em.py
+++ susan/io/em.py
@@ -1,6 +1,6 @@
 ###########################################################################
 # This file is part of the Substack Analysis (SUSAN) framework.
-# Copyright (c) 2018-2021 Ricardo Miguel Sanchez Loayza.
+# Copyright (c) 2018-2023 Ricardo Miguel Sanchez Loayza.
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -16,9 +16,18 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ###########################################################################
 
-from . import mrc
-from . import em
+__all__ = ['read']
 
-__all__ = []
-__all__.extend(['mrc','em'])
+import numpy as _np
+
+def read(filename):
+    em_hdr = _np.fromfile(filename,dtype=_np.uint8 ,count=4)
+    em_siz = _np.fromfile(filename,dtype=_np.uint32,count=3,offset=4)
+    if em_hdr[0] != 6 or em_hdr[3] != 5:
+       raise ValueError('Invalid EM format.')
+
+    data = _np.fromfile(filename,dtype=_np.float32,count=-1,offset=512)
+    data = _np.reshape(data,(em_siz[2],em_siz[1],em_siz[0]))
+
+    return data
 
