8d2c7b9b366d32ba57b44139e7edf5cd87a18804
Ricardo Sanchez <ricardo.sanchez@embl.de>
2023-07-20 23:10:34 +0200

    Reorder mask application/normalization to references

--- src/ref_maps.h
+++ src/ref_maps.h
@@ -89,30 +89,30 @@ public:
             float avg,std;
             if( mask != NULL ) {
                 if( map != NULL ) {
+                    Math::mul(map,mask,numel);
                     Math::get_avg_std(avg,std,map,numel);
                     if( !Math::normalize(map,numel,avg,std) ) {
                         fprintf(stderr,"Error normalizing map %s [m=%f | s=%f]\n",ref_info.map,avg,std);
                         exit(1);
                     }
-                    Math::mul(map,mask,numel);
                 }
 
                 if( half_A != NULL ) {
+                    Math::mul(half_A,mask,numel);
                     Math::get_avg_std(avg,std,half_A,numel);
                     if( !Math::normalize(half_A,numel,avg,std) ) {
                         fprintf(stderr,"Error normalizing map %s [m=%f | s=%f]\n",ref_info.h1,avg,std);
                         exit(1);
                     }
-                    Math::mul(half_A,mask,numel);
                 }
 
                 if( half_B != NULL ) {
+                    Math::mul(half_B,mask,numel);
                     Math::get_avg_std(avg,std,half_B,numel);
                     if( !Math::normalize(half_B,numel,avg,std) ) {
                         fprintf(stderr,"Error normalizing map %s [m=%f | s=%f]\n",ref_info.h2,avg,std);
                         exit(1);
                     }
-                    Math::mul(half_A,mask,numel);
                 }
             }
 
